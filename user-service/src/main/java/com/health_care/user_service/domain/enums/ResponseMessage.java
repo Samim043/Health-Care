5c19a95beb3af0552cde57977e8d1c41c83685dd
Md. Samim Akhter <srsamim043@gmail.com>
2024-11-25 20:34:22 +0600

    Base Format

---
+++ user-service/src/main/java/com/health_care/user_service/domain/enums/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.health_care.user_service.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ResponseMessage {
+
+    ;
+    private String responseCode;
+    private String responseMessage;
+}
